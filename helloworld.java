eda0abbc871fe02aa8cf2a36c04c558cc0667701
UraharaKiske <catchabhinmr@gmail.com>
2018-09-06 16:49:08 +0530

    mONKEYDGARP@123

---
+++ helloworld.java
@@ -0,0 +1,5 @@
+public class helloworld {   // Save as "Hello.java"
+   public static void main(String[] args) {  // program entry point
+      System.out.println("Hello, world!");   // print message
+   }
+}
